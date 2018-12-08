8df62a5f1b6c70b59cce46372dfb7a35107122a2
chris <chrispyprogrammer@gmail.com>
2018-12-08 03:45:05 +0200

    eslint config / test indexjs

--- loginapp/index.js
+++ loginapp/index.js
@@ -1,7 +1,24 @@
 /** @format */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+//import {AppRegistry} from 'react-native';
+//import App from './App';
+//import {name as appName} from './app.json';
+//
+//AppRegistry.registerComponent(appName, () => App);
 
-AppRegistry.registerComponent(appName, () => App);
+
+//==================================================
+
+
+// importing libraries
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+
+
+//creating components
+const App = () => (
+     <Text>This is my first app</Text>
+);
+
+// rendering components to display on the device
+AppRegistry.registerComponent('loginapp', () => App);
