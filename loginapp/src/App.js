84c06e2e22aeb518f6a4e9759a73b72d11bf2205
chris <chrispyprogrammer@gmail.com>
2018-12-10 07:05:05 +0200

    initial login Form build

--- loginapp/src/App.js
+++ loginapp/src/App.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+//import firebase from 'firebase';
+import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  //componentWillMount() {
+  //  firebase.initializeApp({
+  //      apiKey: 'AIzaSyD5mgoyr0EDqrOrlaYYNF9KqhlnnHdQgpU',
+  //      authDomain: 'loginapp-f2dfb.firebaseapp.com',
+  //      databaseURL: 'https://loginapp-f2dfb.firebaseio.com',
+  //      projectId: 'loginapp-f2dfb',
+  //      storageBucket: 'loginapp-f2dfb.appspot.com',
+  //      messagingSenderId: '605563606553' 
+  //  });
+  //}
+
   render() {
-    return (<View>
-      <Text>Some text here</Text>
-    </View>);
+    return (
+    <View>
+      <Header headerText='Fitness App' />
+      <LoginForm />
+    </View>
+    );
   }
 }
 
