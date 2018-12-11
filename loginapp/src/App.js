cd3990b00d52cc5339f36c10e100719165732771
chris <chrispyprogrammer@gmail.com>
2018-12-11 08:52:00 +0200

    firebse auth/work in progress

--- loginapp/src/App.js
+++ loginapp/src/App.js
@@ -1,29 +1,33 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
-//import firebase from 'firebase';
+import firebase from 'firebase';
 import { Header } from './components/common';
-import LoginForm from './components/LoginForm';
+//import LoginForm from './components/LoginForm';
 
 class App extends Component {
-  //componentWillMount() {
-  //  firebase.initializeApp({
-  //      apiKey: 'AIzaSyD5mgoyr0EDqrOrlaYYNF9KqhlnnHdQgpU',
-  //      authDomain: 'loginapp-f2dfb.firebaseapp.com',
-  //      databaseURL: 'https://loginapp-f2dfb.firebaseio.com',
-  //      projectId: 'loginapp-f2dfb',
-  //      storageBucket: 'loginapp-f2dfb.appspot.com',
-  //      messagingSenderId: '605563606553' 
-  //  });
-  //}
+  componentWillMount() {
+    firebase.initializeApp({
+
+      });
+  }
+
 
   render() {
     return (
     <View>
       <Header headerText='LOGO' />
-      <LoginForm />
     </View>
     );
   }
+
+  //render() {
+  //  return (
+  //  <View>
+  //    <Header headerText='LOGO' />
+  //    <LoginForm />
+  //  </View>
+  //  );
+  //}
 }
 
 export default App;
